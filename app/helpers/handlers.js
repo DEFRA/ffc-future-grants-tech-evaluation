17f2cd639ec57cf5fe5020591eb1df157437d7cf
awebber <awebber@awebbers-mbp.home>
2023-12-07 11:44:21 +0000

    Added the basic structure for a simple grant journey

--- app/helpers/handlers.js
+++ app/helpers/handlers.js
@@ -52,18 +52,7 @@ const sendContactDetailsToSenders = async (request, confirmationId) => {
   }
 }
 
-const setTitle = async (title, question, request) => {
-  if (title) {
-    return {
-      ...question,
-      title: title.replace(
-        SELECT_VARIABLE_TO_REPLACE,
-        (_ignore, additionalYarKeyName) =>
-          formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
-      )
-    }
-  }
-}
+
 const processGA = async (question, request) => {
   if (question.ga) {
     if (question.ga.journeyStart) {
@@ -75,345 +64,91 @@ const processGA = async (question, request) => {
   }
 }
 
-const addConsentOptionalData = async (url, request) => {
-  if (url === 'confirm') {
-    const consentOptional = getYarValue(request, 'consentOptional')
-    return getConsentOptionalData(consentOptional)
+const interpolateString = (stringToCheck, request) => {
+  const itemsToReplace = stringToCheck.match(/{{_(.+?)_}}/ig);
+  if (!itemsToReplace || itemsToReplace.length === 0) {
+    return stringToCheck;
+  }
+  itemsToReplace.forEach((item) => {
+    const cleanUpYarKey = RegExp(/{{_(.+?)_}}/ig).exec(item)[1];
+    const yarValue = getYarValue(request, cleanUpYarKey);
+    stringToCheck = stringToCheck.replace(item, yarValue);
+  });
+  return stringToCheck;
+}
+const titleInterpolation = (title, question, request) => {
+  const changedTitle = interpolateString(title, request);
+  return {
+    ...question,
+    title: changedTitle
   }
 }
 
-const addConditionalLabelData = async (
-  question,
-  yarKey,
-  type,
-  request,
-  condHTML
-) => {
-  if (question?.conditionalKey && question?.conditionalLabelData) {
-    const conditional =
-      yarKey === 'businessDetails' ? yarKey : question.conditionalKey
-    condHTML = handleConditinalHtmlData(
-      type,
-      question.conditionalLabelData,
-      conditional,
-      request
-    )
+const labelInterpolation = (label, question, request) => {
+  const labelText = interpolateString(label.text, request);
+  return {
+    ...question,
+    label: {
+      ...question.label,
+      text: labelText
+    }
   }
-  return condHTML
 }
-const isImperableCover = getQuestionAnswer('applying-for', 'applying-for-A2')
-const isPig = getQuestionAnswer('applicant-type', 'applicant-type-A1')
-const isReplaceStore = getQuestionAnswer('project-type', 'project-type-A1')
-const isExistingCover = getQuestionAnswer('existing-cover', 'existing-cover-A1')
 
 const getPage = async (question, request, h) => {
   const {
     url,
-    nextUrlObject,
     type,
     title,
     yarKey,
-    preValidationKeys,
-    preValidationKeysRule,
-    backUrlObject
+    backUrl,
+    nextUrl,
+    label
   } = question
-  let nextUrl = getUrl(nextUrlObject, question.nextUrl, request)
-  let backUrl = getUrl(backUrlObject, question.backUrl, request)
-  const isRedirect = guardPage(
-    request,
-    preValidationKeys,
-    preValidationKeysRule
-  )
-  if (isRedirect) {
-    return h.redirect(startPageUrl)
+
+  if (title) {
+    question = titleInterpolation(title, question ,request);
   }
-  let confirmationId = ''
-  setGrantsData(question, request)
-
-  switch (url) {
-    case 'existing-cover-type':
-      if (getYarValue(request, 'applyingFor') === isImperableCover) {
-        setYarValue(request, 'planningPermission', null)
-        question.backUrl = `${urlPrefix}/reference-cost`
-        question.sidebar.showSidebar = false
-      } else if (getYarValue(request, 'coverType')) {
-        question.backUrl = `${urlPrefix}/cover-type`
-        question.sidebar.showSidebar = true
-      } else if (getYarValue(request, 'projectType') === isReplaceStore) {
-        if (getYarValue(request, 'applicantType') === isPig) {
-          question.backUrl = `${urlPrefix}/pig-capacity-increase-replace`
-          question.sidebar.showSidebar = true
-        } else {
-          question.backUrl = `${urlPrefix}/capacity-increase-replace`
-          question.sidebar.showSidebar = true
-        }
-      } else {
-        if (getYarValue(request, 'applicantType') === isPig) {
-          question.backUrl = `${urlPrefix}/pig-capacity-increase-additional`
-          question.sidebar.showSidebar = true
-        } else {
-          question.backUrl = `${urlPrefix}/capacity-increase-additional`
-          question.sidebar.showSidebar = true
-        }
-      }
-      break
-    case 'separator':
-      if (getYarValue(request, 'coverType')) {
-        if (getYarValue(request, 'existingCover') && getYarValue(request, 'existingCover') === isExistingCover) {
-          question.backUrl = `${urlPrefix}/existing-grant-funded-cover-size`
-        } else {
-          question.backUrl = `${urlPrefix}/cover-size`
-        }
-      } else if (getYarValue(request, 'existingCoverSize')) {
-        question.backUrl = `${urlPrefix}/existing-cover-size`
-      } else {
-        if (getYarValue(request, 'applicantType') === isPig) {
-          if (getYarValue(request, 'projectType') === isReplaceStore) {
-            question.backUrl = `${urlPrefix}/pig-capacity-increase-replace`
-          } else {
-            question.backUrl = `${urlPrefix}/pig-capacity-increase-additional`
-          }
-        } else {
-          if (getYarValue(request, 'projectType') === isReplaceStore) {
-            question.backUrl = `${urlPrefix}/capacity-increase-replace`
-          } else {
-            question.backUrl = `${urlPrefix}/capacity-increase-additional`
-          }
-        }
-      }
-
-    case 'estimated-grant':
-      setYarValue(request, 'estimatedGrant', 'reached')
-      if (getYarValue(request, 'applyingFor') === isImperableCover && getYarValue(request, 'fitForPurpose') === 'No') {
-        backUrl = `${urlPrefix}/grant-funded-cover`
-      }
-    case 'fit-for-purpose':
-      break
-    case 'fit-for-purpose-conditional':
-      if (getYarValue(request, 'applyingFor') === isImperableCover) {
-        question.maybeEligibleContent.isImpermeableCoverOnly = true
-        question.nextUrl = `${urlPrefix}/project-type`
-        nextUrl = getUrl(nextUrlObject, question.nextUrl, request)
-      } else {
-        question.maybeEligibleContent.isImpermeableCoverOnly = false
-      }
-      break
-    default:
-      break
+  if (label) {
+    question = labelInterpolation(label, question ,request);
   }
-
-  if (
-    url === 'potential-amount' &&
-    !getGrantValues(getYarValue(request, 'itemsTotalValue'), question.grantInfo)
-      .isEligible
-  ) {
-    const NOT_ELIGIBLE = { ...question.ineligibleContent, backUrl }
-    gapiService.sendGAEvent(request, { name: gapiService.eventTypes.ELIMINATION, params: {} })
-    return h.view('not-eligible', NOT_ELIGIBLE)
-  }
-
-  // if (url === 'result-page') {
-  //   await gapiService.sendGAEvent({ name: gapiService.eventTypes.ELIGIBILITIES, params: { reference_cost: 'Eligible' }})
-  // }
-
-  await processGA(question, request)
-
-  if (question.maybeEligible) {
-    let { maybeEligibleContent } = question
-    maybeEligibleContent.title = question.title
-    let consentOptionalData
-
-    if ('conditionalText' in maybeEligibleContent) {
-      const value = getYarValue(request, maybeEligibleContent.conditionalText.dependantYarKey)
-      const validationType = maybeEligibleContent.conditionalText.validationType
-      const details = maybeEligibleContent.conditionalText.details
-      if (getYarValue(request, 'solidFractionStorage') != 'Concrete bunker') {
-        maybeEligibleContent.conditionalText.condition = false
-      } else {
-        maybeEligibleContent.conditionalText.condition = !validateAnswerField(value, validationType, details, payload = '')
-      }
-      maybeEligibleContent = {
-        ...maybeEligibleContent,
-        conditionalText: {
-          ...maybeEligibleContent.conditionalText,
-          conditionalPara: maybeEligibleContent.conditionalText.conditionalPara.replace(
-            SELECT_VARIABLE_TO_REPLACE,
-            (_ignore, additionalYarKeyName) =>
-              formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
-          )
-        }
-      }
-    }
-
-    if (maybeEligibleContent.reference) {
-      if (!getYarValue(request, 'consentMain')) {
-        return h.redirect(startPageUrl)
-      }
-      confirmationId = getConfirmationId(request.yar.id)
-
-      // Send Contact details to GAPI
-      await sendContactDetailsToSenders(request, confirmationId)
-
-      maybeEligibleContent = {
-        ...maybeEligibleContent,
-        reference: {
-          ...maybeEligibleContent.reference,
-          html: maybeEligibleContent.reference.html.replace(
-            SELECT_VARIABLE_TO_REPLACE,
-            (_ignore, _confirmatnId) => confirmationId
-          )
-        }
-      }
-      request.yar.reset()
-    }
-
-    maybeEligibleContent = {
-      ...maybeEligibleContent,
-      messageContent: maybeEligibleContent.messageContent.replace(
-        SELECT_VARIABLE_TO_REPLACE,
-        (_ignore, additionalYarKeyName) =>
-          formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
-      )
-    }
-
-    consentOptionalData = await addConsentOptionalData(url, request)
-
-    const MAYBE_ELIGIBLE = {
-      ...maybeEligibleContent,
-      consentOptionalData,
-      url,
-      nextUrl,
-      backUrl
-    }
-    return h.view('maybe-eligible', MAYBE_ELIGIBLE)
-  }
-
-  await setTitle(title, question, request)
 
   const data = getDataFromYarValue(request, yarKey, type)
-
-  let conditionalHtml
-  conditionalHtml = await addConditionalLabelData(
-    question,
-    yarKey,
-    type,
-    request,
-    conditionalHtml
-  )
 
   switch (url) {
     case 'check-details': {
       return h.view(
         'check-details',
-        getCheckDetailsModel(request, question, backUrl, nextUrl)
+        getCheckDetailsModel(request, question)
       )
     }
-    case 'planning-permission-summary': {
-      const evidenceSummaryModel = getEvidenceSummaryModel(
-        request,
-        question,
-        backUrl,
-        nextUrl
+    case 'confirmation': {
+      const confirmationId = getConfirmationId(request.yar.id);
+      return h.view(
+        'confirmation',
+        {
+          url,
+          backUrl,
+          reference: {
+            titleText: "Application complete",
+            html: `Your reference number<br><strong>${confirmationId}</strong>`
+          },
+          confirmationId
+        }
       )
-      if (evidenceSummaryModel.redirect) {
-        return h.redirect(startPageUrl)
-      }
-      return h.view('evidence-summary', evidenceSummaryModel)
-    }
-    case 'score':
-    case 'business-details':
-    case 'agent-details':
-    case 'applicant-details': {
-      return h.view('page', getModel(data, question, request, conditionalHtml))
     }
     default:
       break
   }
 
-  return h.view('page', getModel(data, question, request, conditionalHtml))
+  return h.view('page', getModel(data, question, request))
 }
 
-const clearYarValue = (yarKey, payload, request) => {
-  if (yarKey === 'consentOptional' && !Object.keys(payload).includes(yarKey)) {
-    setYarValue(request, yarKey, '')
-  }
-}
 const createAnswerObj = (payload, yarKey, type, request, answers) => {
   let thisAnswer
   for (let [key, value] of Object.entries(payload)) {
     thisAnswer = answers?.find((answer) => answer.value === value)
-
-    if (key === 'gridReference') value = value.replace(/\s/g, '')
-
-    if (yarKey === 'applicantType' && value !== isPig) setYarValue(request, 'intensiveFarming', null)
-
-    if (yarKey === 'grantFundedCover' && value !== 'Yes, I need a cover') {
-      setYarValue(request, 'coverType', null)
-      setYarValue(request, 'coverSize', null)
-    } else if (yarKey === 'existingCover' && value !== 'Yes') {
-      setYarValue(request, 'fitForPurpose', null)
-      setYarValue(request, 'existingCoverType', null)
-      setYarValue(request, 'existingCoverSize', null)
-    } else if (yarKey === 'fitForPurpose' && value === 'Yes' && getYarValue(request, 'applyingFor') === isImperableCover) {
-      setYarValue(request, 'existingCover', null)
-      setYarValue(request, 'projectType', null)
-      setYarValue(request, 'grantFundedCover', null)
-      setYarValue(request, 'storageType', null)
-      setYarValue(request, 'serviceCapacityIncrease', null)
-      setYarValue(request, 'coverType', null)
-      setYarValue(request, 'coverSize', null)
-    } else if (yarKey === 'separator' && value === 'No') {
-      setYarValue(request, 'separatorType', null)
-      setYarValue(request, 'separatorOptions', null)
-      setYarValue(request, 'gantry', null)
-      setYarValue(request, 'concreteBunkerSize', null)
-      setYarValue(request, 'solidFractionStorage', null)
-    } else if (yarKey === 'separatorType') {
-      setYarValue(request, 'separatorOptions', value)
-    } else if (yarKey === 'gantry' && value === 'Yes') {
-      const tempSeparatorVal = [getYarValue(request, 'separatorOptions')].flat()
-      tempSeparatorVal.push('Gantry')
-      setYarValue(request, 'separatorOptions', tempSeparatorVal)
-    } else if (yarKey === 'gantry' && value === 'No') {
-      const tempSeparatorVal = [getYarValue(request, 'separatorOptions')].flat()
-      setYarValue(request, 'separatorOptions', tempSeparatorVal)
-    } else if (yarKey === 'solidFractionStorage' && value === 'Concrete pad') {
-      const tempSeparatorVal = [getYarValue(request, 'separatorOptions')].flat()
-      // push user entered value
-      tempSeparatorVal.push(value)
-      setYarValue(request, 'separatorOptions', tempSeparatorVal)
-      setYarValue(request, 'concreteBunkerSize', null)
-    } else if (yarKey === 'solidFractionStorage' && value === 'Concrete bunker') {
-      const tempSeparatorVal = [getYarValue(request, 'separatorOptions')].flat()
-
-      // push user entered value
-      tempSeparatorVal.push('Concrete bunker')
-
-      setYarValue(request, 'separatorOptions', tempSeparatorVal)
-    } else if (yarKey === 'solidFractionStorage' && Number(value)) {
-      setYarValue(request, 'concreteBunkerSize', value)
-      const tempSeparatorVal = [getYarValue(request, 'separatorOptions')].flat()
-      if (tempSeparatorVal[tempSeparatorVal.length - 1] === 'Concrete bunker') {
-        tempSeparatorVal.push('Size: ' + value + 'm²')
-        setYarValue(request, 'separatorOptions', tempSeparatorVal)
-      }
-    } else if (yarKey === 'solidFractionStorage' && value === 'I already have short-term storage') {
-      setYarValue(request, 'concreteBunkerSize', null)
-    }
-
-    if (type !== 'multi-input' && key !== 'secBtn') {
-      setYarValue(
-        request,
-        key,
-        key === 'projectPostcode'
-          ? value
-              .replace(DELETE_POSTCODE_CHARS_REGEX, '')
-              .split(/(?=.{3}$)/)
-              .join(' ')
-              .toUpperCase()
-          : value
-      )
-    }
+    setYarValue(request, yarKey, value)
   }
   return thisAnswer
 }
@@ -463,33 +198,24 @@ const showPostPage = async (currentQuestion, request, h) => {
   const {
     yarKey,
     answers,
-    baseUrl,
+    url,
     ineligibleContent,
     nextUrl,
     nextUrlObject,
     title,
-    type
+    type,
+    label
   } = currentQuestion
-  const NOT_ELIGIBLE = { ...ineligibleContent, backUrl: baseUrl }
+  const NOT_ELIGIBLE = { ...ineligibleContent, backUrl: url }
   const payload = request.payload
 
-  let thisAnswer
-  let dataObject
-
-  clearYarValue(yarKey, payload, request)
-  thisAnswer = createAnswerObj(payload, yarKey, type, request, answers)
-
-  handleMultiInput(type, request, dataObject, yarKey, currentQuestion, payload)
+  const thisAnswer = createAnswerObj(payload, yarKey, type, request, answers)
 
   if (title) {
-    currentQuestion = {
-      ...currentQuestion,
-      title: title.replace(
-        SELECT_VARIABLE_TO_REPLACE,
-        (_ignore, additionalYarKeyName) =>
-          formatUKCurrency(getYarValue(request, additionalYarKeyName) || 0)
-      )
-    }
+    currentQuestion = titleInterpolation(title, currentQuestion ,request);
+  }
+  if (label) {
+    currentQuestion = labelInterpolation(label, currentQuestion ,request);
   }
 
   const errors = checkErrors(payload, currentQuestion, h, request)
@@ -498,36 +224,7 @@ const showPostPage = async (currentQuestion, request, h) => {
   }
 
   if (thisAnswer?.notEligible) {
-    await gapiService.sendGAEvent(request, {
-      name: gapiService.eventTypes.ELIMINATION,
-      params: {
-      }
-    })
     return h.view('not-eligible', NOT_ELIGIBLE)
-  } else if (thisAnswer?.redirectUrl) {
-    return h.redirect(thisAnswer?.redirectUrl)
-  }
-
-  if (yarKey === 'serviceCapacityIncrease') {
-    if (getYarValue(request, 'grantFundedCover') === getQuestionAnswer('grant-funded-cover', 'grant-funded-cover-A1')) {
-      return h.redirect(`${urlPrefix}/cover-type`)
-    } else if (getYarValue(request, 'existingCover') && getYarValue(request, 'existingCover') === isExistingCover) {
-      return h.redirect(`${urlPrefix}/existing-cover-type`)
-    } else {
-      return h.redirect(`${urlPrefix}/separator`)
-    }
-  }
-
-  if (yarKey === 'grantFundedCover') {
-    if (getYarValue(request, 'applyingFor') === isImperableCover && getYarValue(request, 'fitForPurpose') === 'No') {
-      return h.redirect(`${urlPrefix}/estimated-grant`)
-    } else {
-      if (getYarValue(request, 'applicantType') === isPig) {
-        return h.redirect(`${urlPrefix}/existing-cover-pig`)
-      } else {
-        return h.redirect(`${urlPrefix}/existing-cover`)
-      }
-    }
   }
 
   return h.redirect(
